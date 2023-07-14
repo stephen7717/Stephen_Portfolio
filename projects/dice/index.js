743499292c2df3abed8b36bc65933fb897198c36
stephen7717 <74173366+stephen7717@users.noreply.github.com>
2023-07-14 17:20:38 +0800

    Update index.js

--- projects/dice/index.js
+++ projects/dice/index.js
@@ -11,19 +11,19 @@ location.reload();
 }
 
 let backBtn = document.querySelector(".back-button").addEventListener("click",function(){
-    window.location.href = "/index.html";
+    window.location.href = "./index.html";
 });
 
 var diceOne = Math.floor((Math.random()*6)+1);
 var diceTwo = Math.floor((Math.random()*6)+1);
 
 var randomDice = "dice" + diceOne +".png"; 
-var randomImage = ("/images/")+randomDice ;
+var randomImage = ("./images/")+randomDice ;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImage);
 
 var randomDice2 = "dice" + diceTwo +".png"; 
-var randomImage2 = ("/images/")+randomDice2;
+var randomImage2 = ("./images/")+randomDice2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImage2);
 
